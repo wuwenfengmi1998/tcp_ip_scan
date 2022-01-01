c0bd30000335ea6f09e9e89b1e1b18ae16f64ae4
kevin <kevin@lmve.net>
2022-01-01 20:53:22 +0800

    重新布局

--- ip_sacaner/widget.cpp
+++ ip_sacaner/widget.cpp
@@ -1,11 +1,25 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDebug>
+#include <QTcpSocket>  //通信套接字
+
+
+void tray_scan()
+{
+    qDebug("1");
+
+}
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+     qDebug("hello world");
+
+    connect(ui->stard_scan,&QPushButton::pressed,this,tray_scan);//扫描按钮被按下
+
 }
 
 Widget::~Widget()
